15cc0bd1c31c69eed0216f6f189e59b227d9db1b
Parth Jhanwar <parthjhanwar711@gmail.com>
2026-03-11 16:01:58 +0530

    Made backend Acciseble from IP

--- app/main.py
+++ app/main.py
@@ -199,3 +199,12 @@ def change_password(
     db.refresh(user)
 
     return {"message": "Password updated successfully"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
