680a81b21bbd4e4edb029b0c42b84ee6a1af2cb1
Parth Jhanwar <parthjhanwar711@gmail.com>
2026-03-11 16:25:27 +0530

    Updated Environment varialble

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from dotenv import load_dotenv
+load_dotenv()
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
